38b57acb975173c86212d330240dfbf819d03601
asyrafzain <81062088+asyrafz@users.noreply.github.com>
2024-07-19 19:30:46 +0800

    Create BasicYolov8Inference.py
    stage1

---
+++ BasicYolov8Inference.py
@@ -0,0 +1,33 @@
+import cv2
+import torch
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    
+    print(f"Using device: {device}")
+
+    cap = cv2.VideoCapture(0)
+
+    #model = YOLO('yolov8n.pt')
+    model = YOLO('yolov8x04072024.pt')
+    model.to(device)
+
+    while cap.isOpened():
+        success, frame = cap.read()
+        if success:
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            results = model(frame_rgb, device=device)
+
+            annotated_frame = cv2.cvtColor(results[0].plot(), cv2.COLOR_BGR2RGB)
+
+            cv2.imshow("Y8", annotated_frame)
+
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+        else:
+            break
+cap.release()
+cv2.destroyAllWindows()
